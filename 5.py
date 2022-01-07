99ffec04528d0d2368f426144d5242e0270653a0
Bj11111 <95857589+Bj11111@users.noreply.github.com>
2022-01-07 12:18:17 +0800

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,5 +1,5 @@
 # def display_(board):
-  def print_board():
+def print_board():
     st.write(dict[7], '|' , dict[8] , '|', dict[9])
     st.write('- + - + -') 
     st.write(dict[4], '|' , dict[5] , '|', dict[6])
