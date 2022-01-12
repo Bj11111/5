1ad48d06d6023d515ec61cca57e532d2989cd952
Bj11111 <95857589+Bj11111@users.noreply.github.com>
2022-01-12 14:08:29 +0800

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,4 +1,6 @@
 import random
+import cv2
+import numpy as np
 import streamlit as st
 
 class Die:
@@ -48,3 +50,11 @@ if has_won:
     ## 如果想再玩一次 請按F5!
     ## 祝大家都可以all pass開心過寒假
     '''
+st.title('彩蛋')
+uploaded_file = st.file_uploaded(" ",type = "jpg")
+if uploaded_file is not None:
+    file_bytes = np.asarray(bytearray(uploaded_file.raed()),dtype=np.uint8)
+    opencv_image = cv2.imdecode(file_bytes,1)
+    st.image(opencv_image,channels="BGR")
+    cv2.imwrite('test.jpg',opencv_image)
+    
